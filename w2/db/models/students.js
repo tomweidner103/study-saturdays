a08094db2723cb2cc47c31623c579c56a8aed882
Thomas Weidner <tomweidner103@gmail.com>
2018-11-17 22:19:28 -0800

    firts

--- w2/db/models/students.js
+++ w2/db/models/students.js
@@ -3,6 +3,36 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+        hooks:{
+            type: Sequelize.STRING,
+            beforeBulkCreate: function(){
+                this.firstName = this.firstName.charAt(0).toUpperCase() + this.firstname.slice(1)
+                return this.firstName
+            }
+        },
+        firstName: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            }
+        },
+        fullName: {
+            type: Sequelize.STRING,
+            get () {
+                return this.getDataValue('firstName') + ' ' + this.getDataValue('lastName')
+            }
+        },
+
+});
 
 module.exports = Student;
--- w2/test/01_models.test.js
+++ w2/test/01_models.test.js
@@ -45,7 +45,7 @@ describe('Models', function() {
         });
       });
 
-      xit('requires `firstName`', () => {
+      it('requires `firstName`', () => {
         student.firstName = null;
         return student.validate().then(
           () => {
@@ -55,7 +55,7 @@ describe('Models', function() {
         );
       });
 
-      xit('requires `lastName`', () => {
+      it('requires `lastName`', () => {
         student.lastName = null;
         return student.validate().then(
           () => {
@@ -65,7 +65,7 @@ describe('Models', function() {
         );
       });
 
-      xit('requires `email`', () => {
+      it('requires `email`', () => {
         student.email = null;
         return student.validate().then(
           () => {
@@ -75,7 +75,7 @@ describe('Models', function() {
         );
       });
 
-      xit('requires `email` to be in an email form', () => {
+      it('requires `email` to be in an email form', () => {
         student.email = 'hola world';
         return student.validate().then(
           () => {
@@ -94,7 +94,7 @@ describe('Models', function() {
 
     describe('options definition', () => {
       describe('`fullName` virtual field', () => {
-        xit('returns the full name', () => {
+        it('returns the full name', () => {
           expect(student.fullName).to.equal(`${firstName} ${lastName}`);
         });
       });
@@ -109,7 +109,7 @@ describe('Models', function() {
           });
         });
 
-        xit('capitalizes the first letter of the first and last name before save to the DB', () => {
+        it('capitalizes the first letter of the first and last name before save to the DB', () => {
           return newStudent.save().then(savedStudent => {
             expect(savedStudent.firstName).to.equal('Charles');
             expect(savedStudent.lastName).to.equal('Xavier');
